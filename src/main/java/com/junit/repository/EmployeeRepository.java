619b1c7c09f9a14f8db56df61292a9b3335b30fc
Rahul Gupta <rahuldevit2000@gmail.com>
2026-01-06 21:53:47 +0530

    Update EmployeeRepository.java
    Update Repository

--- src/main/java/com/junit/repository/EmployeeRepository.java
+++ src/main/java/com/junit/repository/EmployeeRepository.java
@@ -1,9 +1,6 @@
 package com.junit.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.junit.bean.Employee;
-
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
 }
+
